e27203112d578e0415d51b88e34edb508513915e
Omar Rasheed <oyr2@cornell.edu>
2020-05-21 04:09:35 -0700

    Initial commit

---
+++ Example/MyTest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyTest
+//
+//  Created by omarrasheed on 05/21/2020.
+//  Copyright (c) 2020 omarrasheed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
